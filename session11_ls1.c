513edfc07f0333655bf45947d10b8bfc75cbbbbf
pthongnhung <pthongnhung13122006@gmail.com>
2024-12-09 19:23:51 +0700

    Add files via upload

---
+++ session11_ls1.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int calculateTotal(){
+	int a=3;
+	int b=4; 
+	int total=a+b; 
+	printf("tong cua so %d va %d la %d \n",a,b,total);
+    return a+b; 
+}
+int main(){
+	calculateTotal();
+	return 0; 
+}
